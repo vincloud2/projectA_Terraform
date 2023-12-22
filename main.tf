862bb5a8e4497fe6a39e96b2dd4fd33db6a05939
vincloud2 <128999303+vincloud2@users.noreply.github.com>
2023-12-22 21:15:12 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "terraform-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "vinod_2023_nov_new"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-04e862208a52f4b86"]
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
